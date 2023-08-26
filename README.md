138f0e221caabf3bf3f16c54700521fc32426da4
luarocks <luarocks@onionmail.org>
2023-08-26 08:07:26 +0300

    Init

---
+++ README.md
@@ -0,0 +1,35 @@
+# Pixel Fonts
+
+![Screenshot](Screenshot.png)
+
+This mod installs or removes multilingual pixel fonts: [LanaPixel] (default) and
+[Monogram] (monospace, for the console). Both fonts are distributed under a free
+license, just like this mod.
+
+After installing or uninstalling the fonts, exit the game and disable this mod.
+It functions as a font installer for the entire engine, and keeping it enabled
+in your games doesn't make sense.
+
+---
+
+Этот мод устанавливает или удаляет многоязычные пиксельные шрифты: [LanaPixel]
+(по умолчанию) и [Monogram] (моноширинный, для консоли). Оба шрифта
+распространяются под свободной лицензией, как и этот мод.
+
+После установки или удаления шрифтов выйдите из игры и отключите этот мод. Он
+работает как установщик шрифтов для всего движка и держать его всегда включенным
+в ваших играх нет смысла.
+
+---
+
+This mod was created in [Minetestia Forge].
+
+All my mods uses [Minetest LSP-API] annotations, [StyLua] and [Luacheck].
+
+[LanaPixel]:
+  https://opengameart.org/content/lanapixel-localization-friendly-pixel-font
+[Monogram]: https://datagoblin.itch.io/monogram
+[Minetestia Forge]: https://github.com/orgs/minetestia/repositories
+[Minetest LSP-API]: https://github.com/minetest-toolkit/minetest-lsp-api
+[StyLua]: https://github.com/JohnnyMorganz/StyLua
+[Luacheck]: https://github.com/mpeterv/luacheck
---
+++ init.lua
@@ -0,0 +1,41 @@
+local current_mod_name = minetest.get_current_modname()
+local current_mod_path = minetest.get_modpath(current_mod_name)
+
+local lanapixel_path = current_mod_path .. "/fonts/LanaPixel.ttf"
+local monogram_path = current_mod_path .. "/fonts/Monogram.ttf"
+
+minetest.register_on_player_receive_fields(function(player, formname, fields)
+  if formname ~= "pixel_fonts" then return end
+  local player_name = player:get_player_name()
+  local settings = minetest.settings
+  if fields.install then
+    settings:set("font_path", lanapixel_path)
+    settings:set("font_size", 22)
+    settings:set("mono_font_path", monogram_path)
+    settings:set("chat_font_size", 32)
+  elseif fields.uninstall then
+    settings:remove "font_path"
+    settings:remove "font_size"
+    settings:remove "mono_font_path"
+    settings:remove "chat_font_size"
+  end
+  settings:write()
+  minetest.chat_send_player(
+    player_name,
+    "Please exit the game and disable this mod.\n"
+      .. "Exiting is necessary for the fonts to be fully applied,\n"
+      .. "and there's no need to keep the mod enabled."
+  )
+  minetest.close_formspec(player_name, "pixel_fonts")
+end)
+
+local function openFontFormspec(player_name)
+  local formspec = "size[6,4]"
+    .. "button[2,2;2,1;install;Install fonts]"
+    .. "button[2,3;2,1;uninstall;Uninstall fonts]"
+  minetest.show_formspec(player_name, "pixel_fonts", formspec)
+end
+
+minetest.register_on_joinplayer(
+  function(player) openFontFormspec(player:get_player_name()) end
+)
